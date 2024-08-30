daf96e726031d2ac7a184cc67ff0344d7bc28890
NickVendetti <nrvendetti@gmail.com>
2024-08-30 00:07:29 -0400

    Final commit

--- app.py
+++ app.py
@@ -2,10 +2,15 @@ from flask import Flask, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from models import Pet, db, connect_db 
-from forms import AddPetForm
+from forms import AddPetForm, EditPetForm
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = 'static/uploads'
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/adopt'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'mysecretkey'
@@ -18,11 +23,23 @@ def show_homepage():
     pets = Pet.query.all()
     return render_template('homepage.html', pets=pets)
 
+def save_image(image):
+    if image:
+        filename = secure_filename(image.filename)
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        image.save(filepath)
+        return filepath 
+    return None
+
 @app.route('/add', methods=["GET", "POST"])
 def add_pet():
     form = AddPetForm()
 
     if form.validate_on_submit():
+        photo_url = form.photo_url.data
+        if form.photo.data:
+            photo_url = save_image(form.photo.data)
+
         new_pet = Pet(
             name=form.name.data,
             species=form.species.data,
@@ -37,3 +54,18 @@ def add_pet():
     
     return render_template('add_pet.html', form=form)
 
+@app.route('/<int:pet_id>', methods=["GET", "POST"])
+def show_edit_pet(pet_id):
+    pet = Pet.query.get_or_404(pet_id)
+    form = EditPetForm(obj=pet) # pre-fill the form with the current pet data
+
+    if form.validate_on_submit():
+        pet.photo_url = form.photo_url.data
+        if form.photo.data:
+            pet.photo_url = save_image(form.photo.data)
+        pet.notes = form.notes.data
+        pet.available = form.available.data
+        db.session.commit()
+        return redirect(url_for('show_homepage'))
+    
+    return render_template('edit_pet.html', pet=pet, form=form)
--- forms.py
+++ forms.py
@@ -1,10 +1,20 @@
+from flask_wtf.file import FileField, FileRequired, FileAllowed
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, TextAreaField, BooleanField
-from wtforms.validators import InputRequired, Optional, URL, NumberRange
+from wtforms.validators import InputRequired, Optional, URL, NumberRange, AnyOf
 
 class AddPetForm(FlaskForm):
     name = StringField("Pet Name", validators=[InputRequired()])
-    species = StringField("Species", validators=[InputRequired()])
+    species = StringField("Species", validators=[InputRequired(), AnyOf(['cat', 'dog', 'porcupine'], message="Species must be 'cat', 'dog', or 'porcupine'.")])
     photo_url = StringField("Photo URL", validators=[Optional(), URL()])
-    age = IntegerField("Age", validators=[Optional(), NumberRange(min=0, max=30)])
+    age = IntegerField("Age", validators=[Optional(), NumberRange(min=0, max=30, message="Age must be between 0 and 30.")])
     notes = TextAreaField("Notes", validators=[Optional()])
+    photo = FileField("Upload Photo", validators=[
+        Optional(),
+        FileAllowed(['jpg', 'png'], 'Images only!')
+    ])
+
+class EditPetForm(FlaskForm):
+    photo_url = StringField("Photo URL", validators=[Optional(), URL()])
+    notes = TextAreaField("Notes", validators=[Optional()])
+    available = BooleanField("Available")
